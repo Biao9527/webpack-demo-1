fca6f8c609947ccd4726049fa3f8edbf554745dd
biao <1247821135@qq.com>
2021-12-14 13:45:31 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[chunkhash].js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'My App',
+    template: 'src/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
